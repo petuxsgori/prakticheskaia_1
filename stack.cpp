36b59278ae0288655f08b45a5ac5a4790998cb68
petuxsgori <47988382+petuxsgori@users.noreply.github.com>
2019-05-25 12:10:11 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,8 @@
+
 #include "pch.h"
 #include <string>
 #include <iostream>
-#include <set>
+#include <stack>
 using namespace std;
 class date {
 public:
@@ -18,52 +19,48 @@ public:
 		this->name_month = data.name_month;
 		this->year = data.year;
 	}
+	friend ostream& operator<< (ostream &out,const date &data) {
+		out << data.day << " " << data.name_month << " " << data.year << endl;
+		return out;
+	}
 };
 
 int main()
 {
 	setlocale(0, "rus");
-	int a;
 	bool ka=0;
-	set <int> mn;
+	int a;
+	stack <int> mn;
 	date ke[5];
-	set <date> vremia;
-	cout << "введите 5 элементов из множества целых чисел" << endl;
+	stack <date> vremia;
+	cout << "введите 5 элементов в стэка целых чисел" << endl;
 	for (int i = 0; i < 5; i++) {
 		cin >> a;
-		mn.insert(a);
+		mn.push(a);
 	}
-	cout << "удалили 3-й элемент" << endl;
-	mn.erase(3);
-	cout << "теперь множество выглядит так:" << endl;
-	for (auto i: mn) {
-		cout << i<<" ";
+	cout << "удалили верхний элемент"<<endl;
+	mn.pop();
+	cout << "размер стека стал- " << mn.size() << endl;
+	cout << "теперь стэк выглядит так:" << endl;
+	while (!mn.empty()) {
+		cout << mn.top()<<" ";
+		mn.pop();
 	}
-	cout << "какой элемент вы хотите проверить?" << endl;
-	cin >> a;
-	for (auto i : mn) {
-		if (i==a)cout << " он есть" << endl;
-		ka = 1;
-	}
-	if(!ka)cout << " его нету" << endl;
-	ka = 0;
-	cout << "размер множества- " << mn.size() << endl;
-	cout << "теперь введите 5 дат,которые хотите внести в множество"<<endl;
+	cout << "теперь стек пустойv" << endl;
+	cout << "размер стека- " << mn.size() << endl;
+	cout << "теперь введите 5 дат,которые хотите внести в множествоv"<<endl;
 	for (int i = 0; i < 5; i++) {
 		cin >> ke[i].day;
 		cin >> ke[i].name_month;
 		cin >> ke[i].year;
-		vremia.insert(ke[i]);
+		vremia.push(ke[i]);
 	}
-	vremia.erase(ke[2]);
-	cout << "проверка на то,осталась ли вторая дата после удаления ее из множества" << endl;
-	for (auto i : vremia) {
-		if (i==ke[2])cout << "дата  есть" << endl;
-	}
-	if(!ka)cout << " даты нету" << endl;
-	cout << "оставшиеся даты множества:" << endl;
-	for (auto i:vremia) {
-		cout << i.day << " " << i.name_month << " " << i.year << endl;
+	vremia.pop();
+	cout << "вытолкнули верхний элемент стэкаv" << endl;
+	cout << " теперь размер стэка стал-"<<vremia.size();
+	cout << "оставшиеся даты стэка и их удаление:" << endl;
+	while (!vremia.empty()) {
+		cout<<vremia.top()<<endl;
+		vremia.pop();
 	}
 }
-
